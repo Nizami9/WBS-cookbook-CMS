47d3cee0db47ad8b33a84bb7e2b5692299d215fb
roxibi <106161599+roxibi@users.noreply.github.com>
2022-11-10 21:01:45 +0100

    style flex footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,13 +1,18 @@
 import React from "react";
+<<<<<<< HEAD
 import Reviews from "../reviews.json";
 import "../footer.css"
+=======
+import Reviews from "../Reviews.json";
+import "./footer.css"
+>>>>>>> aa80823 (style flex footer)
 
 function Footer() {
   let { reviews } = Reviews;
 
   return (
     <div className="footerComponent">
-      <p>Footer</p>
+      <p></p>
       {reviews.map((r) => (
         <div>
           <h3>{r.quote}</h3>
